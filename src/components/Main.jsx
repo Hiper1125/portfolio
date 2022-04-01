36db983ffdf7b99d9436da0185b69e7f9e46541d
Samuele Guerrini <guerrini.work@gmail.com>
2022-04-01 14:04:09 +0200

    Home and 404 Routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import ProjectsPage from "./pages/projects";
 import AboutPage from "./pages/about";
 import ContactPage from "./pages/contact";
+import NotFoundPage from "./pages/404";
+import { Navigate } from "react-router-dom";
 
 const Main = () => {
   return (
-      <Routes>
-        <Route exact path="/" element={<ProjectsPage />}></Route>
-        <Route exact path="/projects" element={<ProjectsPage />}></Route>
-        <Route exact path="/about" element={<AboutPage />}></Route>
-        <Route exact path="/contact" element={<ContactPage />}></Route>
-      </Routes>
+    <Routes>
+      <Route exact path="/" element={<Navigate to="/projects" />}></Route>
+      <Route exact path="/projects" element={<ProjectsPage />}></Route>
+      <Route exact path="/about" element={<AboutPage />}></Route>
+      <Route exact path="/contact" element={<ContactPage />}></Route>
+      <Route exact path="/404" element={<NotFoundPage />}></Route>
+      <Route path="*" element={<Navigate to="/404" />}></Route>
+    </Routes>
   );
 };
 
---
+++ src/components/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="w-full h-auto">
+      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[14rem] font-bold">
+        404
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
